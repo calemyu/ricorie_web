2f6bffd378027bc77548d699c494332c4ab94b8c
Calemyu <enricoreinhardi37@gmail.com>
2024-01-16 06:48:15 +0900

    audition section img added

--- src/assets/index.js
+++ src/assets/index.js
@@ -3,8 +3,9 @@ import logo from "./logo.png";
 import menu from "./menu.svg";
 import close from "./close.svg"
 import cardmatatabi from "./cardmatatabi.png"
+import audition from "./audprocess.png"
 
 
 
 
-export {cardaono, logo, menu, close, cardmatatabi};
+export {cardaono, logo, menu, close, cardmatatabi, audition};
--- src/components/Audition.jsx
+++ src/components/Audition.jsx
@@ -6,19 +6,29 @@ import { SectionWrapper } from "../hoc";
 import { styles } from "../styles";
 import { talents } from "../constants";
 import { fadeIn, textVariant } from "../utils/motion";
+import { audition } from '../assets' 
 
 const Audition = () => {
   return (
-    <motion.div
-      className="md:w-3/5"
-      variants={textVariant()}
-    >
-      <h2 className={`${styles.sectionHeadText}`}>Audition</h2>
+      <motion.div 
+      className="flex flex-wrap justify-between"
+      variants={textVariant()}>
+        <p className={`${styles.sectionHeadText}`}> Audition</p>
+
+        <div className='flex items-center '>
+        
+          <img src={audition} alt="audition" className='min-w-[60px] h-auto'/>
+
+        </div>
+      </motion.div>
+      
+
+      
 
     
     
     
-    </motion.div>
+    
   )
 }
 
